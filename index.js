8570fcdccc92ba9d723802e1ffa042a089e3c8ce
Jonas Laß <johnny.jones@gmx.net>
2023-05-12 14:32:59 +0200

    firstpart

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // phew… not a lot going on here. Please add some code!
+const bookmark = document.querySelector('[data-js="bookmark"]');
+
+bookmark.addEventListener("click", () => {
+  bookmark.classList.toggle("bookmark--active");
+});
+
+/* const answer = document.querySelector('data-js="card__answer');
+const answerButton = document.querySelector('data-js="');
+ */
+const answer = document.querySelector('[data-js="card__answer"]');
+const answerButton = document.querySelector('[data-js="card__button-answer"]');
+
+answerButton.addEventListener("click", () => {
+  answer.classList.toggle("card__answer--active");
+  if (answer.classList.contains("card__answer--active")) {
+    answerButton.textContent = "Hide answer";
+  } else {
+    answerButton.textContent = "Show answer";
+  }
+});
